17676e579bf4e65e7ec7af9a5fac7145d5e06601
Mbalhakim <mb.alhakim@gmail.com>
2025-02-03 06:44:49 +0100

    config fix dor delpoyment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
   basePath: '/leetcode-solutions', 
   trailingSlash: true, // Ensures that URLs end with a slash, required by GitHub Pages
   reactStrictMode: true, // generally recommended for better debugging
